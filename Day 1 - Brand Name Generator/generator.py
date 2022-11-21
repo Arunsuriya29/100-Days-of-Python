23b968d1efeb915e6ac033c915e8e3fce9978e3f
Webjango03 <webjango03@gmail.com>
2022-11-21 21:39:35 +0530

    Brand Name Generator.

--- Day 1 - Brand Name Generator/generator.py
+++ Day 1 - Brand Name Generator/generator.py
@@ -1,3 +1,7 @@
-name = "Jack"
+print ("Welcome to the brand name generator.")
 
-print(name)
+city = input("What's name of the city you grew up in?\n")
+
+pets = input("What's your pet's name?\n")
+
+print("Your brand name could be " + city + " " + pets)
