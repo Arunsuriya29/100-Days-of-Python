853f66bd2d4b26087c98d1b18a2d10bc318a4322
Webjango03 <webjango03@gmail.com>
2022-12-08 06:26:29 +0530

    ticket update.

--- Day_3/Ticket.py
+++ Day_3/Ticket.py
@@ -12,9 +12,12 @@ if height >= 120:
         bill = 5
         print("Child tickets are $5.")
 
-    elif age <=18:
+    elif age <= 18:
         bill = 7
         print("Youth tickets are $7.")
+
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!")
 
     else:
         bill = 12
